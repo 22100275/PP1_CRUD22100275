ca7462bd1bc50ab975b60a03280b8566bdd5ed07
gnh09042 <22100275@handong.ac.kr>
2023-09-03 19:26:33 +0900

    version. 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("***영단어 마스터***\n");
+        System.out.println("\n********************\n");
+        System.out.println("1. 모든 단어 보기\n");
+        System.out.println("2. 수준별 단어 보기\n");
+        System.out.println("3. 단어 검색\n");
+        System.out.println("4. 단어 추가\n");
+        System.out.println("5. 단어 수정\n");
+        System.out.println("6. 단어 삭제\n");
+        System.out.println("7. 파일 저장\n");
+        System.out.println("0. 나가기\n");
+
+
+    }
+}
